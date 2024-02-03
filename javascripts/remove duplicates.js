6037fd2a28e50805adaee97943c2fd9af7a2e68d
himu <satirtharoy2003@gmail.com>
2024-02-03 21:03:16 +0600

    remove duplicate.js more optimized

--- javascripts/remove duplicates.js
+++ javascripts/remove duplicates.js
@@ -1,9 +1,11 @@
-function removeDupli (arr) {
-  const unique = [];
-  for(let item of arr) {
-    !unique.includes(item) && unique.push(item);
-  }
-  return unique;
-}
+// function removeDupli (arr) {
+//   const unique = [];
+//   for(let item of arr) {
+//     !unique.includes(item) && unique.push(item);
+//   }
+//   return unique;
+// }
+
+const removeDupli = (arr) => arr.filter((item,i)=>arr.lastIndexOf(item)==i);
 
 console.log(removeDupli(['himu', 'satirtha', 'roy', 'himu', 'satirtha', 'himu', 1,3,2,1,3,4,2,1]));
